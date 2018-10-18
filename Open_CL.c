d50570b84bd9870e5ba13b131d4496a679f163d5
Klas Holmgren <klas0holmgren@gmail.com>
2018-10-18 20:27:54 +0200

    sad

--- Open_CL.c
+++ Open_CL.c
@@ -18,12 +18,11 @@ int main(int argc, char* argv[]){
   const double maxIter = strtol(argv[5]+2, &endpt,10);
 
   double boxes[boxHeight+2][boxWidth+2];
-  int test[boxHeight+2][boxWidth+2];
+  __int * test;
 
   for(int i = 0; i < boxHeight+2; i++){
     for(int j = 0; j < boxWidth+2; j++){
       boxes[i][j] = 0;
-      test[i][j] = 0;
     }
   }
 
@@ -122,12 +121,7 @@ int main(int argc, char* argv[]){
 
   clFinish(queue);
 
-  for(int j = 1; j < boxHeight+1; j++){
-    for(int k = 1; k < boxWidth+1; k++){
-      printf("   %f   ",test[j][k]);;
-    }
-  printf("\n");
-  }
+  printf("  Läst från GPU %i   ",test[j][k]);;
 
   clReleaseCommandQueue(queue);
   clReleaseContext(context);
