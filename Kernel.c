93b699d8d974745a6a457483619aad8f2a7e59d3
Klas Holmgren <klas0holmgren@gmail.com>
2018-10-18 19:59:28 +0200

    sad

--- Kernel.c
+++ Kernel.c
@@ -1,10 +1,6 @@
 
 __kernel void heat_diffuse(__global int * boxes) {
   int id = get_global_id(0);
-
-  printf("Hej work item %i här!",id);
-
-  boxes[(id+100) %10][(id+100)%20] = id;
   boxes[3][3] = 1337;
 
 }
