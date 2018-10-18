120c4c46dcc88635e283504bdc11e667b08c6d7b
Klas Holmgren <klas0holmgren@gmail.com>
2018-10-18 02:31:20 +0200

    fixes

--- Open_CL.c
+++ Open_CL.c
@@ -2,13 +2,26 @@
 #include <stdio.h>
 #include <CL/cl.h>
 
-int main(void){
+int main(int argc, char* argv[]){
+
   //declartaions
-  int data = 100;
-  int DATA_SIZE = 1000;
-  int LENGTH = 1000;
+
+  char * endpt;
+
+  const int boxHeight = strtol(argv[1], &endpt,10);
+  const int boxWidth = strtol(argv[2], &endpt,10);
+  const double iniHeat = strtol(argv[3]+1, &endpt,10);
+  const double diffu = strtol(argv[4]+2, &endpt,10);
+  const double maxIter = strtol(argv[5]+2, &endpt,10);
 
   //OpenCL related declartaions
+  //Läser tillbaka data i denna
+  global float * data = 100;
+  int DATA_SIZE = 1000;
+  int HEIGHT = boxHeight;
+  int WIDTH = boxWidth;
+
+
   cl_context context;
   cl_device_id device;
   cl_program program;
@@ -16,28 +29,21 @@ int main(void){
   cl_mem buffer;
   cl_command_queue  queue;
 
-
-
-
   // Setup OpenCL
-  int nrDevis = 1;
-
-  //cl_device_id *devices
-
-
   clGetDeviceIDs(NULL,CL_DEVICE_TYPE_GPU, 1, &device, NULL);
 
-  //clGetDeviceIDs(NULL, CL_DEVICE_TYPE_GPU, nrDevis, &device, NULL);
   context = clCreateContext(NULL, nrDevis, &device, NULL, NULL, NULL);
   queue = clCreateCommandQueue(context, device, (cl_command_queue_properties)0, NULL);
 
   //Define our kernel:
   char *source = {
-    "kernel void heat_diffusion(global float *data) {\n"
-    "KOD HÄR!"
-    " int id get_global_id(0);\n"
-    " data[id] = sin(data[id]);\n"
-    "}\n"
+    "void kernel heat_diffuse(global float *boxes) {"
+      //get thread number
+      "int id = get_global_id(0);"
+
+      "printf('Hej work item %i här!',id);"
+      "boxes[j][k] = boxes[j][k] + diffu*((boxes[j-1][k] + boxes[j+1][k] + boxes[j][k-1] + boxes[j][k+1])/4 - boxes[j][k]);"
+    "}"
   };
 
   // Compile the kernel
@@ -53,12 +59,18 @@ int main(void){
   cl_int clEqnWriBuff = clEnqueueWriteBuffer(queue, buffer, CL_FALSE, 0, DATA_SIZE, data, 0, NULL, NULL);
 
   // Execute the kernel
-  cl_int clKerlAr = clSetKernelArg(kernel, 0, sizeof(buffer), &buffer);
-  size_t global_dimemsions[] = {LENGTH,0,0};
-  clEnqueueNDRangeKernel(queue, kernel, 1, NULL, global_dimemsions, NULL, 0, NULL, NULL);
+  //sätter argument 0 i kernel till buffer
+  clSetKernelArg(kernel, 0, sizeof(buffer), &buffer);
+
+  //Antalet thrads som ska köras
+  size_t global_dimemsions[] = {HEIGHT,WIDTH,0};
+  size_t local_dimemsions[] = {3,3,0};
+
+  //clEnqueueNDRangeKernel(queue, kernel, GLOBAL DIM, LOCAL DIM, global_dimemsions, NULL, 0, NULL, NULL);
+  clEnqueueNDRangeKernel(queue, kernel, 2, NULL, global_dimemsions, local_dimemsions, 0, NULL, NULL);
 
   //Read back the results
-  clEnqueueReadBuffer(queue, buffer, CL_FALSE, 0, sizeof(cl_float)*LENGTH, data, 0, NULL, NULL);
+  clEnqueueReadBuffer(queue, buffer, CL_FALSE, 0, sizeof(cl_float)*HEIGHT, data, 0, NULL, NULL);
 
   //Wait for ecerything to finish
   clFinish(queue);
